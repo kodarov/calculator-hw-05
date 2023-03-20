71215f5e99f8b7bfddf0acfd0f4ae4506d0d04d4
kodarov_salavat <kodarov@gmail.com>
2023-03-21 01:48:52 +0400

    1st commit

---
+++ src/main/java/pro/sky/calculatorhw05/CalculatorHw05Application.java
@@ -0,0 +1,13 @@
+package pro.sky.calculatorhw05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorHw05Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculatorHw05Application.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/calculatorhw05/CalculatorHw05ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.calculatorhw05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorHw05ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
