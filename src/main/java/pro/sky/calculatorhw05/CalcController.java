f17e3a73fcf32004c90a8d78923742b7787deec3
kodarov_salavat <kodarov@gmail.com>
2023-03-22 00:43:35 +0400

    calc

--- src/main/java/pro/sky/calculatorhw05/CalcController.java
+++ src/main/java/pro/sky/calculatorhw05/CalcController.java
@@ -12,8 +12,43 @@ public class CalcController {
         this.calcService = calcService;
     }
 
-    @GetMapping(path ="/calculator")
-    public String calculator(){
-        return calcService.welcome();
+    @GetMapping(path = "/calculator")
+    public String calculator() {
+        return "Добро пожаловать в калькулятор.";
+    }
+
+    @GetMapping(path = "/calculator/plus")
+    public String plus(@RequestParam(value = "num1", required = false) Double a, @RequestParam(value = "num2", required = false) Double b) {
+        if (a == null || b == null ) {
+            return "ОШИБКА!";
+        }
+        return "num1 + num2 = " + calcService.plus(a, b);
+    }
+
+    @GetMapping(path = "/calculator/minus")
+    public String minus(@RequestParam(value = "num1", required = false) Double a, @RequestParam(value = "num2", required = false) Double b) {
+        if (a == null || b == null) {
+            return "ОШИБКА!";
+        }
+        return "num1 - num2 = " + calcService.minus(a, b);
+    }
+
+    @GetMapping(path = "/calculator/multiply")
+    public String multiply(@RequestParam(value = "num1", required = false) Double a, @RequestParam(value = "num2", required = false) Double b) {
+        if (a == null || b == null) {
+            return "ОШИБКА!";
+        }
+        return "num1 * num2 = " + calcService.multiply(a, b);
+    }
+
+    @GetMapping(path = "/calculator/divide")
+    public String divide(@RequestParam(value = "num1", required = false) Double a, @RequestParam(value = "num2", required = false) Double b) {
+        if (a == null || b == null) {
+            return "ОШИБКА!";
+        }
+        if (b == 0) {
+            return "Деление на 0!";
+        }
+        return "num1 / num2 = " + calcService.divide(a, b);
     }
 }
--- src/main/java/pro/sky/calculatorhw05/CalcService.java
+++ src/main/java/pro/sky/calculatorhw05/CalcService.java
@@ -4,7 +4,20 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CalcService {
-    public String welcome(){
-        return "Добро пожаловать в калькулятор.";
+    public double plus(Double a, Double b) {
+        return a + b;
     }
+
+    public double minus(Double a, Double b) {
+        return a - b;
+    }
+
+    public double multiply(Double a, Double b) {
+        return a * b;
+    }
+
+    public double divide(Double a, Double b) {
+        return a / b;
+    }
+
 }
